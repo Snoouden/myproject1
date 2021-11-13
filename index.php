468a01a36d1bdc9b4fb5888307de0acf24b5b339
Snoouden <you@example.com>
2021-11-13 12:32:36 +0100

    box-section

--- index.php
+++ index.php
@@ -101,68 +101,50 @@
   
 
     
-    <div id="advantages"><!-- #advantages Begin -->
+        <div id="advantages"><!-- #advantages Begin -->
+       
+            <div class="container"><!-- container Begin -->
+                
+                <div class="same-height-row"><!-- same-height-row Begin -->
 
-     <div class="container"><!-- container Begin -->
+                    <?php 
+                    
+                    $get_boxes = "select * from boxes_section";
+                    $run_boxes = mysqli_query($con,$get_boxes);
 
-        <div class="same-heigh-row"><!-- same-heigh-row Begin -->
+                    while($run_boxes_section=mysqli_fetch_array($run_boxes)){
 
-             <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                        $box_id = $run_boxes_section['box_id'];
+                        $box_title = $run_boxes_section['box_title'];
+                        $box_desc = $run_boxes_section['box_desc'];
+                    
+                    ?>
+                        
+                        <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                            
+                            <div class="box same-height"><!-- box same-height Begin -->
+                                
+                                <div class="icon"><!-- icon Begin -->
+                                    
+                                    <i class="fa fa-heart"></i>
+                                    
+                                </div><!-- icon Finish -->
+                                
+                                <h3><a href="#"><?php echo $box_title; ?></a></h3>
+                                
+                                <p> <?php echo $box_desc; ?> </p>
+                                
+                            </div><!-- box same-height Finish -->
+                            
+                        </div><!-- col-sm-4 Finish -->
 
-                  <div class="box same-height"><!-- box same-height Begin -->
-
-                       <div class="icon"><!-- icon Begin -->
-
-                            <i class="fa fa-heart"></i>
-
-                       </div><!-- icon Finish -->
-
-                       <h3><a href="#">Best Offer</a></h3>
-                       <p>We know to provide the best posible service ever never</p>
-
-                  </div><!-- box same-height Finish -->
-
-             </div><!-- col-sm-4 Finish -->
-
-              <div class="col-sm-4"><!-- col-sm-4 Begin -->
-
-                <div class="box same-height"><!-- box same-height Begin -->
-
-                    <div class="icon"><!-- icon Begin -->
-
-                         <i class="fa fa-tag"></i>
-
-                    </div><!-- icon Finish -->
-
-                     <h3><a href="#">Best Prices</a></h3>
-                     <p>Compare us with another store site, who have the best prices</p>
-
-                  </div><!-- box same-height Finish -->
-
-             </div><!-- col-sm-4 Finish -->
-
-             <div class="col-sm-4"><!-- col-sm-4 Begin -->
-
-                  <div class="box same-height"><!-- box same-height Begin -->
-
-                      <div class="icon"><!-- icon Begin -->
-
-                          <i class="fa fa-thumbs-up"></i>
-
-                      </div><!-- icon Finish -->
-
-                      <h3><a href="#">100% Original </a></h3>
-                      <p>We offer you only the best and original products with us</p>
-
-                  </div><!-- box same-height Finish -->
-
-              </div><!-- col-sm-4 Finish -->
-
-        </div><!-- same-heigh-row Finish -->
-
-
-      </div><!-- container Finish -->
-     </div><!-- #advantages Finish -->
+                        <?php    } ?>
+                    
+                </div><!-- same-height-row Finish -->
+                
+            </div><!-- container Finish -->
+       
+        </div><!-- #advantages Finish -->
  
    
     <div id="hot"><!-- #hot Begin -->
